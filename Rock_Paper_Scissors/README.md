65fbbd8b7f5c411a5da5fa4cdd4d88306f31149c
FrostyPillsMan <adamtangmch@gmail.com>
2023-09-25 14:49:49 +0800

    Added New Project to repository

---
+++ Rock_Paper_Scissors/README.md
@@ -0,0 +1,54 @@
+
+# Rock Paper Scissors
+
+Welcome to the Rock Paper Scissors Game, a simple Python command-line game that challenges you to guess a strongest weapon on dominating your opponent.
+
+## Intructions:
+1. **Installation**: There is no specific installation required. Ensure you have Python installed on your system, and you can execute the script by running `python name_file.py` in your terminal.
+
+2. **How to Play**:
+   - Enter your first name and last name when prompted.
+   - Given option on choosing single-letter(R/P/S) or full_character(Scissors/Rock/Paper).
+   - Enter your computer name and its selected pick.
+   - The game will provide feedback, providing you the win/lose output.
+   - The game will display a scoreboard between you and computer result.
+   - After game has ended, you can decide to play again or exit the game.
+
+3. **Features**:
+   - User Initialization:
+   - The user is prompted to enter their first name and last name to create a username.
+   - The username is displayed.
+
+**Game Loop**:
+- The game is wrapped in an infinite loop, allowing the user to play multiple rounds.
+
+**User Input**:
+- The user can input their choice using either a single-letter key (s for Scissors,     p for Paper, r for Rock) or the full word (Scissors, Paper, Rock). 
+- Input is case-insensitive.
+- The program handles invalid input and prompts the user to try again.
+
+**Computer's Choice**:
+- The computer randomly selects its choice (Scissors, Paper, Rock).
+
+**Game Outcome**:
+- The user's choice and the computer's choice are compared, and the winner (or a stalemate) is determined based on Rock, Paper, Scissors rules.
+- The game outcome (win, lose, or stalemate) is displayed to the user.
+- The user's and computer's win counts are updated accordingly.
+
+**Scoreboard Display**:
+- After each round, the program displays a scoreboard showing the user's and computer's names and their respective win counts.
+
+**Play Again**:
+- The user is asked if they want to play another round.
+- If the user chooses to continue, the game restarts.
+- If the user chooses to quit, the program exits with a thank-you message.
+
+4. **Usage**
+    - Execute the script by running python name_file.py.
+    - Follow the on-screen instructions to play the game.
+
+Enjoy your childhood games, fellows😀
+
+
+
+
---
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -0,0 +1,113 @@
+# library module
+import random as rand_hand_guesses
+
+
+choice_mapping_1 = {
+    "s": "Scissors",
+    "p": "Paper",
+    "r": "Rock",
+}
+# storing data for user_key input.
+
+choice_mapping_2 = ["scissors", "paper", "rock"]
+# storing data for user_string type input.
+
+
+def scoreboard(user_name, user_wins, computer_name, computer_wins):
+    name_header = ["Username", "User Wins", "Computer Name", "Computer Wins"]
+
+    score_data = [user_name, user_wins, computer_name, computer_wins]
+
+    # display name header and score together.
+    for title, value in zip(name_header, score_data):
+        print(f"{title}: {value}")
+        pass
+
+
+def starting_rps():
+    print("-------------------------------------------")
+    print("| welcome to the rock_paper_scissors game |".title())
+    print("-------------------------------------------")
+
+    firstname = input("Enter your firstname : ").lower()
+    lastname = input("Enter your lastname : ").lower()
+    user_name = " ".join([firstname, lastname])
+    # Combine firstname and lastname to the user_name.
+    print("Username :", user_name)
+
+    rand_value = 2
+    # declare random number at last index position.
+    user_wins = 0
+    computer_wins = 0
+
+    while True:
+        print("------------------------")
+        message_display = "[--| ✂ Scissors", " 📜 Paper", " 🥌 Rock |--]"
+        print(message_display)
+
+        user_choice = input(
+            "Option1 : type (s/p/r) | Option2 : type (Scissors/Paper/Rock)\n"
+            "Which hand choices would you like to pick?\n"
+            " (Scissors[s], Paper[p], Rock[r]): "
+        ).lower()
+
+        # first condition checks user key input[s/p/r].
+        # second condition checks user string type input.
+        if user_choice in choice_mapping_1:
+            user_select = choice_mapping_1[user_choice]
+        elif user_choice in choice_mapping_2:
+            user_select = user_choice
+        else:
+            print("Invalid input!")
+            continue
+            # continues the loop if user input not matches.
+
+        print("You chose: -->", user_select)
+        print("\t##---Next---##")
+        break
+        # end the control flow loop.
+
+    rand_hand = rand_hand_guesses.randint(0, rand_value)
+    # generates random hand_guesses from 0-2
+    computer_pick = choice_mapping_2[rand_hand]
+    # computer using user_string type input.
+    computer_name = input("Enter Computer name :").capitalize()
+    print("{} picked --> {}".format(computer_name, computer_pick))
+    # uses string formatting to display concise value.
+
+    if user_select.lower() == "scissors" and computer_pick == "paper":
+        print("You have Won!")
+        user_wins += 1
+    elif user_select.lower() == "paper" and computer_pick == "rock":
+        print("You have Won!")
+        user_wins += 1
+    elif user_select.lower() == "rock" and computer_pick == "scissors":
+        print("You have Won!")
+        user_wins += 1
+    elif user_select.lower() == computer_pick:
+        print("Stalemate!")
+        user_wins += 0.5
+        computer_wins += 0.5
+    else:
+        print("You have lost!")
+        computer_wins += 1
+
+    scoreboard(user_name, user_wins, computer_name, computer_wins)
+    # display the scoreboard header.
+
+    # prompt the user to play again.
+    user_play_again = input("Do you want to play again? (y/n):").lower()
+    if user_play_again.lower() != "y":
+        print("Thank you for your time! Appreciate it😃")
+        quit()
+        # exit the program.
+    else:
+        starting_rps()
+        # reset the program.
+
+
+# run the script as a main program.
+if __name__ == "__main__":
+    starting_rps()
+
+
