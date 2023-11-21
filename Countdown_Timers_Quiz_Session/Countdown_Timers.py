3476223fd288799028c7fa00b801efaecd91b4cc
Heiserberg Stan <sventhestrike567@gmail.com>
2023-11-21 14:20:34 +0800

    Added new Project

---
+++ Countdown_Timers_Quiz_Session/Countdown_Timers.py
@@ -0,0 +1,78 @@
+import time
+import functools
+
+
+# Define a list of questions with their corresponding answers
+questions = [
+    ("How many Countries are available in the Asean Market?", "10"),
+    ("Who is the Richest Person in the World?", "Elon Musk"),
+    ("Who is the Current President of the United Kingdom?", "Penny Mordaunt"),
+    ("When did Rainbow Six Siege was official released", "2015"),
+    ("Where is Riot Games Headquarter located?", "Los Angeles"),
+]
+
+
+def countdown():
+    title = ["Welcome", "To", "The", "Time_Game"]
+    title_display = functools.reduce(lambda x, y,: x + " " + y, title)
+    print(title_display, "\n")
+
+    fstname = input("Enter your firstname : ").capitalize()
+    lstname = input("Enter your lastname : ").capitalize()
+    fllname = " ".join([fstname, lstname])
+    print("Fullname : ", fllname, "\n")
+
+    user_play = input("Do you want to play? ->(y/n) ").lower()
+
+    if user_play != "y":
+        print("Thank you very much!")
+        exit()
+    else:
+        print("Let's play!\n")
+        # continue the game process.
+
+    print("You have to justify the right answers for five questions given?")
+    user_play = input(("Countdown begins when you say (yes) :))")).lower()
+
+    if user_play == "yes":
+        time_taken = 3
+        while time_taken > 0:
+            print(f"Time remaining: {time_taken:02d} seconds")
+            time.sleep(1)
+            time_taken -= 1
+        print("countdown begins now!".upper())
+    else:
+        print("Ahhhh! You miss your opportunity! :(")
+        quit()
+
+    player_score = 0
+
+    # Iterate over the questions
+    for question, correct_answer in questions:
+        answer = input(question)
+        if answer.lower() == correct_answer:
+            print("You got it right!")
+            player_score += 1
+        else:
+            print("Better luck next time.")
+
+    total_questions = len(questions)
+    # Declare new variable of total_questions equal to return length of questions list.
+    percentage = (player_score / total_questions) * 100
+
+    print(f"\nYou got {player_score} out of {total_questions} questions correct!")
+    print(f"Your score: {percentage}%.")
+
+
+    user_play_again = input("Do you want to play again? (y/n):").lower()
+    if user_play_again.lower() != "y":
+        print("Thank you for your time! Appreciate it😃")
+        quit()
+        # exit the program.
+    else:
+        countdown()
+        # reset the program.
+
+
+if __name__ == "__main__":
+    countdown()
---
+++ Countdown_Timers_Quiz_Session/README.md
@@ -0,0 +1,36 @@
+
+# Countdown Timers on Quiz Session
+
+Welcome to the Countdown Timers, a simple Python command-line where shows you basic quiz-answering based on the countdown given.
+
+1. **How to Play**:
+   - Login your fullname ->firstname & lastname
+   - User prompt been given to play the game (Yes/No).
+   - If you choose to play:
+     - Guidance statement will be given to the player.
+     - User enter "yes" and countdown starts at 3 and game begins.
+     - Answering five questions given from the screen.
+   - Result of the player will be shown after the game.
+
+2. **Features**:
+   - The game can be played multiple times.
+   - Option to exit the game at any time.
+   - Score tracking for the total percentage of the score.
+   - User-friendly prompts and game approachment.
+
+3. **Lesson Notes**:
+   - Lambda -> anonymous function that declares arguments & expression.
+   - functools -> iterates over each item and return a single value.
+   - join -> combines both variables/arguments together.
+   - time -> module that gives time method to delay speed-task.
+
+## Usage
+
+1. Execute the script by running `Countdown_Timers.py`.
+
+2. Follow the on-screen instructions to play the game.
+
+Enjoy playing the Countdown Timers, Sir!
+
+
+
