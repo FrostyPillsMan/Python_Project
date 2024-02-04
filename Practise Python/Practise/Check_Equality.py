fe830c6a07902e69f9ea14a80e404c5b0bf11e60
Heiserberg Stan <sventhestrike567@gmail.com>
2024-02-04 16:05:19 +0800

    Update Changes

--- Practise Python/Practise/Check_Equality.py
+++ Practise Python/Practise/Check_Equality.py
@@ -1,7 +1,7 @@
 a = [1, 2, 3]
 b = [1, 2, 3]
 
-print(a == b)
+print(a==b)
 # equal(true)
 
 print(id(a), id(b))
@@ -15,7 +15,7 @@ d = c
 print(id(c), id(d))
 
 print(c is d)
-print(c == d)
+print(c==d)
 
 # All Identical variables are equal but not all Equal variables are identical
 
