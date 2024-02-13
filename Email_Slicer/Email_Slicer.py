e8ac6380372335c871191d210ff0db308e7c482e
Heiserberg Stan <sventhestrike567@gmail.com>
2024-02-13 21:36:10 +0800

    Update Changes

--- Email_Slicer/Email_Slicer.py
+++ Email_Slicer/Email_Slicer.py
@@ -7,12 +7,14 @@ extract_pattern = r"([\w]+)@([\w]+\.[\w]+)"
 matched_string = re.match(extract_pattern, user_email)
 # Checks for matching between extract pattern and the email address.
 
-username = matched_string.group(1)
-# Returns the string from first subgroups index
-domain = matched_string.group(2)
-# Returns the string from second subgroups index
+if matched_string:
+    username = matched_string.group(1)
+    # Returns the string from first subgroups index
+    domain = matched_string.group(2)
+    # Returns the string from second subgroups index
 
-print(f"Extraction of the Username: {username}")
-print(f"Extraction of the Domain: {domain}")
+    print(f"Extraction of the Username: {username}")
+    print(f"Extraction of the Domain: {domain}")
 
-
+else:
+    print("Invalid email address provided.")
