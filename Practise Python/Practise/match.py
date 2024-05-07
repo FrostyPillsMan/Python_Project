dd4ae2916d394c6b52a73384bb1a1aa68058bebb
Heiserberg Stan <sventhestrike567@gmail.com>
2024-05-07 21:54:19 +0800

    Details Added

---
+++ Practise Python/Practise/match.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+try:
+    color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+except ValueError as err:
+    print(f"{err} is not right colour!")
+finally:
+    print("Good Choice!")
+
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
+
+
