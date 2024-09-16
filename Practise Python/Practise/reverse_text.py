f7396d99a84bbf646d613e8dc66881b83c6f8e22
Heiserberg Stan <sventhestrike567@gmail.com>
2024-09-16 14:38:24 +0800

    Updated Reverse Scenario Title from Both Sides

--- Practise Python/Practise/reverse_text.py
+++ Practise Python/Practise/reverse_text.py
@@ -1,12 +1,12 @@
+# Scenario 1
 def junior_text(text):
     return "".join(reversed(text))
 
 print(junior_text("Adam"))
 
+# Scenario 2
 def senior_text(text_1):
     return text_1[::-1]
 
 print(senior_text("Tan"))
-
-
 
