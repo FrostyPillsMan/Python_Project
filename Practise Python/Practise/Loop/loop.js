8278029cdb1ed99f3ccca711a2a69a4d27c74cbb
Heiserberg Stan <sventhestrike567@gmail.com>
2024-11-21 21:52:09 +0800

    Javascript Loops On Loop Folder

---
+++ Practise Python/Practise/Loop/loop.js
@@ -0,0 +1,10 @@
+// Traditional loop
+let numbers = [1, 2, 3, 4, 5, 6]
+for (let i = 0; i < numbers.length; i +=1) {
+    console.log(numbers[i])
+}
+
+// Loop Formula
+// for (initializer; condition; iterator)
+    // body
+
